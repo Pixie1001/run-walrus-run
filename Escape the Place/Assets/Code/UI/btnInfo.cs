c4b0e1688d8631bc81758daec95946347447d0c9
Pixie1001 <45550419+Pixie1001@users.noreply.github.com>
2019-09-29 02:33:27 +1000

    Button proof of concept

---
+++ Escape the Place/Assets/Code/UI/btnInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnInfo : MonoBehaviour
+{
+    public GameObject legend;
+
+    protected void Start() {
+    }
+
+    public void ToggleInfo () {
+        if (!legend.activeSelf) {
+            legend.SetActive(true);
+        }
+        else {
+            legend.SetActive(false);
+        }
+    }
+}
