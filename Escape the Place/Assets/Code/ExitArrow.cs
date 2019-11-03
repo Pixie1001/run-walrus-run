12931d2be5097be2c3f9343547963e2b4e13a176
Pixie1001 <45550419+Pixie1001@users.noreply.github.com>
2019-11-03 18:30:57 +1100

    Exit arrow

---
+++ Escape the Place/Assets/Code/ExitArrow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitArrow : MonoBehaviour
+{
+    float targetTime = 4f;
+    float currTime = 0f;
+    // Use this for initialization
+
+    // Update is called once per frame
+    protected void Update() {
+        if (currTime <= targetTime) {
+            currTime += Time.deltaTime;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+}
